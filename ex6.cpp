53fff873914551a44aeec7fa3f6670fe74c05ced
loccloccc <166293427+loccloccc@users.noreply.github.com>
2024-11-26 10:24:53 +0700

    BTVN ss5

--- ex6.cpp
+++ ex6.cpp
@@ -7,18 +7,19 @@ int main()
 	scanf("%d",&num1);
 	printf("Moi ban nhap so thu hai . ");
 	scanf("%d",&num2);
-	printf("           CALCULATOR");
-	printf("\n1.Tong 2 so : ");
-	printf("\n2.Hieu 2 so : ");
-	printf("\n3.Tich 2 so : ");
-	printf("\n4.Thuong 2 so : ");	
-	printf("\n5.Thoat : ");
+	
 	tong = num1 + num2 ;
 	hieu = num1 - num2;
 	tich = num1 * num2;
 	thuong = num1 / num2;  
 	while(answer != 5)
 	{
+		printf("           CALCULATOR");
+		printf("\n1.Tong 2 so : ");
+		printf("\n2.Hieu 2 so : ");
+		printf("\n3.Tich 2 so : ");
+		printf("\n4.Thuong 2 so : ");	
+		printf("\n5.Thoat : ");
 		printf("\nMoi ban chon :  ");
 		scanf("%d",&answer);
 		switch(answer)
